872330fd8444218cc2abeeb7cc971dde83a52da1
Lionseun Ling <lionseun@gmail.com>
2025-07-29 07:06:51 +0800

    add simple resp

--- QueryRestful/src/main/kotlin/io/github/lios/mysti/query/QueryEngineController.kt
+++ QueryRestful/src/main/kotlin/io/github/lios/mysti/query/QueryEngineController.kt
@@ -3,6 +3,7 @@ package io.github.lios.mysti.query
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 import javax.sql.DataSource
@@ -13,7 +14,7 @@ class QueryEngineController {
     private val database: DataSource? = null
 
     @PostMapping("/query/sql")
-    fun sql(@RequestBody req: QueryRequest): QueryResponse {
+    fun sql(@RequestBody req: QueryRequest, @RequestParam ignoreColumnName: Boolean): QueryResponse {
         database?.connection.use { con ->
             con?.prepareStatement(req.sql)!!.use { ps ->
                 if (!ps.execute()) {
@@ -27,39 +28,41 @@ class QueryEngineController {
                     val metaData = rs.metaData!!
                     val columnCount = metaData.columnCount
                     val columnMetas = (1..columnCount).associate {
-                        metaData.getColumnLabel(it)!! to mapOf(
-                            "type" to metaData.getColumnTypeName(it)!!,
-                            "meta" to ColumnMeta(
-                                autoIncrement =  metaData.isAutoIncrement(it),
-                                caseSensitive =  metaData.isCaseSensitive(it),
-                                searchable =  metaData.isSearchable(it),
-                                currency =  metaData.isCurrency(it),
-                                nullable =  metaData.isNullable(it),
-                                signed =  metaData.isSigned(it),
-                                columnDisplaySize =  metaData.getColumnDisplaySize(it),
-                                columnLabel =  metaData.getColumnLabel(it)!!,
-                                columnName =  metaData.getColumnName(it)!!,
-                                schemaName =  metaData.getSchemaName(it)!!,
-                                precision =  metaData.getPrecision(it),
-                                scale =  metaData.getScale(it),
-                                tableName =  metaData.getTableName(it)!!,
-                                catalogName =  metaData.getCatalogName(it)!!,
-                                columnType =  metaData.getColumnType(it),
-                                columnTypeName =  metaData.getColumnTypeName(it)!!,
-                                readOnly =  metaData.isReadOnly(it),
-                                writable =  metaData.isWritable(it),
-                                definitelyWritable =  metaData.isDefinitelyWritable(it),
-                                columnClassName =  metaData.getColumnClassName(it)!!,
-                            )
+                        metaData.getColumnLabel(it)!! to ColumnMeta(
+                            autoIncrement =  metaData.isAutoIncrement(it),
+                            caseSensitive =  metaData.isCaseSensitive(it),
+                            searchable =  metaData.isSearchable(it),
+                            currency =  metaData.isCurrency(it),
+                            nullable =  metaData.isNullable(it),
+                            signed =  metaData.isSigned(it),
+                            columnDisplaySize =  metaData.getColumnDisplaySize(it),
+                            columnLabel =  metaData.getColumnLabel(it)!!,
+                            columnName =  metaData.getColumnName(it)!!,
+                            schemaName =  metaData.getSchemaName(it)!!,
+                            precision =  metaData.getPrecision(it),
+                            scale =  metaData.getScale(it),
+                            tableName =  metaData.getTableName(it)!!,
+                            catalogName =  metaData.getCatalogName(it)!!,
+                            columnType =  metaData.getColumnType(it),
+                            columnTypeName =  metaData.getColumnTypeName(it)!!,
+                            readOnly =  metaData.isReadOnly(it),
+                            writable =  metaData.isWritable(it),
+                            definitelyWritable =  metaData.isDefinitelyWritable(it),
+                            columnClassName =  metaData.getColumnClassName(it)!!,
                         )
                     }
-
-                    val rset = mutableListOf<Map<String, Any>>()
-                    while (rs.next()) {
-                        val row = (1..columnCount).associate {
-                            metaData.getColumnLabel(it)!! to rs.getObject(it)
+                    var rows = mutableListOf<Any>()
+                    if (!ignoreColumnName) {
+                        while (rs.next()) {
+                            val row = (1..columnCount).associate {
+                                metaData.getColumnLabel(it)!! to rs.getObject(it)
+                            }
+                            rows.add(row)
                         }
-                        rset.add(row)
+                    } else {
+                        while (rs.next()) {
+                            rows.add((1..columnCount).map { rs.getObject(it) }.toList())
+                        }
                     }
 
                     return QueryResponse().apply {
@@ -68,7 +71,7 @@ class QueryEngineController {
                         i18Key = ""
                         data = QueryResult().apply {
                             columns =  columnMetas
-                            results = rset.toList()
+                            results = rows.toList()
                             queryId = UUID.randomUUID().toString()
                             severity = ""
                             suggestion = ""
@@ -94,7 +97,7 @@ class QueryResponse {
 }
 
 class QueryResult {
-    var columns: Map<String, Map<String, Any>> = emptyMap()
+    var columns: Map<String, ColumnMeta> = emptyMap()
     var results: List<Any> = emptyList()
     var queryId: String = ""
     var severity: String = ""
